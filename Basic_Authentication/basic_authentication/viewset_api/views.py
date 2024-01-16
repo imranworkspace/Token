33bc1d876b78cb7edb66d223af487818c1647c54
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-01-16 16:32:28 +0530

    Session with permission  - IsAuthenticated,IsAuthenticatedOrReadOnly,DjangoModelPermissions

--- Basic_Authentication/basic_authentication/viewset_api/views.py
+++ Basic_Authentication/basic_authentication/viewset_api/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from .serializers import StudentSerializers
 from .models import StudentModel 
-from rest_framework.permissions import AllowAny,IsAdminUser,IsAuthenticated
-from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import AllowAny,IsAdminUser,IsAuthenticated,IsAuthenticatedOrReadOnly,DjangoModelPermissions
+from rest_framework.authentication import SessionAuthentication
 # Create your views here.
 from rest_framework import viewsets
 class StudentModelAdminView(viewsets.ModelViewSet):
     queryset = StudentModel.objects.all()
     serializer_class = StudentSerializers
-    authentication_classes=[BasicAuthentication]
-    permission_classes=[IsAdminUser]
+    authentication_classes=[SessionAuthentication]
+    permission_classes=[DjangoModelPermissions]
 
